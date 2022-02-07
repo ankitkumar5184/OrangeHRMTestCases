33a3145638cc61aa045ac679057e10ec6aefb53d
DELL <ankit.kumar@infostride.com>
2022-02-07 11:01:58 +0530

    LoginTest

--- src/test/java/Test/LoginPageTest.java
+++ src/test/java/Test/LoginPageTest.java
@@ -1,5 +1,6 @@
 package Test;
 
+import Pages.DashBoardPage;
 import Pages.LoginPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
@@ -7,20 +8,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class LoginPageTest {
-    WebDriver driver;
-    LoginPage objLogin;
+public class LoginPageTest extends SetUp {
+    @Test
+    public void testLogin() throws InterruptedException
+    {
 
-    @BeforeTest
-    public void SetUp(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get("https://opensource-demo.orangehrmlive.com/index.php");
-    }
-    @Test(priority = 0)
-    public void Login_Test(){
-        objLogin = new LoginPage(driver);
-        objLogin.loginToHrm("Admin","admin123");
-    }
+    LoginPage.enterUsername("Admin");
+    LoginPage.enterPassword("admin123");
 
+    DashBoardPage dashboardPage=LoginPage.clickLoginBtn();
+    dashboardPage.clickWelcomeAdmin();
+    loginPage= dashboardPage.clickLogout();
+
+}
 }
